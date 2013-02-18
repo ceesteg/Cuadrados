1058229dd1517cb127070019d3b58428c65df77c
César Estebas Gómez <ceesteg@gmail.com>
2013-02-18 17:49:58 +0100

    Implementado cierre

--- Cuadrados/src/com/example/cuadrados/MainActivity.java
+++ Cuadrados/src/com/example/cuadrados/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.cuadrados;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button bt = (Button)findViewById(R.id.button2);
+		bt.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				finish();
+			}
+		});
 	}
 
 	@Override
