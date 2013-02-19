56aec32bd178bccb6955b218ba920adaa9a48edd
César Estebas Gómez <ceesteg@gmail.com>
2013-02-19 16:47:07 +0100

    Cambios para Gonzalo

--- Cuadrados/src/com/example/cuadrados/MainActivity.java
+++ Cuadrados/src/com/example/cuadrados/MainActivity.java
@@ -2,25 +2,43 @@ package com.example.cuadrados;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private boolean sound = true;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button bt = (Button)findViewById(R.id.button2);
-		bt.setOnClickListener(new View.OnClickListener() {
+		Button btExit = (Button)findViewById(R.id.button2);
+		btExit.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				finish();
 			}
+		});
+		
+		Button btSound = (Button)findViewById(R.id.button3);
+		btSound.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		        // TODO Auto-generated method stub
+		        Button bt = (Button)findViewById(R.id.button3);
+		        if(bt.getBackground().equals(R.drawable.consonido)){
+			        bt.setBackgroundResource(R.drawable.sinsonido);
+		        } else{
+		        	bt.setBackgroundResource(R.drawable.consonido);
+		        }
+		    }
 		});
 	}
 
@@ -30,5 +48,6 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
 
 }
