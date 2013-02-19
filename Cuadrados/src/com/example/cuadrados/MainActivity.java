80e8d2038ed948cb8f73be529527a1295a45a1a5
César Estebas Gómez <ceesteg@gmail.com>
2013-02-19 17:55:23 +0100

    Versión estable 19/02/2013

--- Cuadrados/src/com/example/cuadrados/MainActivity.java
+++ Cuadrados/src/com/example/cuadrados/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.cuadrados;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
