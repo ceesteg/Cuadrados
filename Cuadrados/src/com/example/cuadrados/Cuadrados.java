d902a8b16efe0fc11847b72ac28fe0fc0c811ac7
Cesar <Cesar@Cesar-PC>
2013-02-25 03:05:43 +0100

    Añadida funcionalidad del sonido de la aplicación

--- Cuadrados/src/com/example/cuadrados/Cuadrados.java
+++ Cuadrados/src/com/example/cuadrados/Cuadrados.java
@@ -1,16 +1,35 @@
 package com.example.cuadrados;
 
 import android.app.Application;
+import android.media.MediaPlayer;
 
 public class Cuadrados extends Application {
 
+	private MediaPlayer mp;
 	private boolean sound = true;
 
-	public boolean sound() {
+	public boolean soundImg() {
 		return sound;
 	}
 
-	public void setSound(boolean s) {
+	public void setSoundImg(boolean s) {
 		sound = s;
+	}
+	
+	public void startSound(int resId){
+		mp = MediaPlayer.create((Cuadrados) getApplicationContext(), resId);
+		mp.start();
+	}
+	
+	public void resumeSound(){
+		mp.start();
+	} 
+	
+	public void pauseSound(){
+		mp.pause();
+	}
+	
+	public void stopSound(){
+		mp.stop();
 	}
 }
--- Cuadrados/src/com/example/cuadrados/MainActivity.java
+++ Cuadrados/src/com/example/cuadrados/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cuadrados;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -8,11 +9,15 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Cuadrados application = (Cuadrados)getApplicationContext();
+		application.startSound(R.raw.sonidofondo);
 		
 		Button btExit = (Button)findViewById(R.id.button2);
 		btExit.setOnClickListener(new View.OnClickListener() {
@@ -30,12 +35,14 @@ public class MainActivity extends Activity {
 		        // TODO Auto-generated method stub
 		    	Cuadrados application = (Cuadrados)getApplicationContext();
 		        Button bt = (Button)findViewById(R.id.button3);
-				if(application.sound()){
+				if(application.soundImg()){
+					application.pauseSound();
 					bt.setBackgroundResource(R.drawable.sinsonido);
-					application.setSound(false);
+					application.setSoundImg(false);
 				} else{
+					application.resumeSound();
 				    bt.setBackgroundResource(R.drawable.consonido);
-				    application.setSound(true);
+				    application.setSoundImg(true);
 				}
 		    }
 		});
@@ -48,5 +55,11 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	@Override
+	public void onStop(){
+		Cuadrados application = (Cuadrados)getApplicationContext();
+		application.stopSound();
+		super.onStop();
+	}
 
 }
